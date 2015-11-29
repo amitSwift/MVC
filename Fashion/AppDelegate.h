e876e2e71b5f69346d8d4d28c06a7addf87a2bf0
Lakhwinder Singh <lakhwindersingh@lakhwinders-macbook-air.local>
2015-11-29 15:59:39 +0530

    Initial Commit

---
+++ Fashion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fashion
+//
+//  Created by Lakhwinder Singh on 29/11/15.
+//  Copyright © 2015 lakh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fashion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fashion
+//
+//  Created by Lakhwinder Singh on 29/11/15.
+//  Copyright © 2015 lakh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
